4bdd0c02d8f753070df4cdfd8796f90647b0361e
Jessica Gladstone <jcg321@comcast.net>
2019-02-18 23:10:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +8,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+ 
+  get '/articles/new' do
+    erb :new
   end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    erb :index
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+      @article = Article.find(params[:id])
+      @article.name = params[:name]
+      @article.content = params[:content]
+      @article.save
+      erb :show
+    end
+
+  delete '/articles/:id/delete' do
+      @article = Article.find(params[:id])
+      @article.destroy
+      erb :deleted
+    end
 end
