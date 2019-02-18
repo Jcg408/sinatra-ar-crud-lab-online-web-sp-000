97f6fb55e16161fc424e0ba09df31e7333efafef
Jessica Gladstone <jcg321@comcast.net>
2019-02-18 23:47:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,16 +12,15 @@ class ApplicationController < Sinatra::Base
   get '/articles/new' do
     erb :new
   end
+  
+  
 
   post '/articles' do
     @article = Article.create(title: params[:title], content: params[:content])
     erb :index'
   end
 
-  get '/articles' do
-    @articles = Article.all
-    erb :index
-  end
+ 
 
   get '/articles/:id' do
     @article = Article.find(params[:id])
